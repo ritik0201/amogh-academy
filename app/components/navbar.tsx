b81908b2db7863a9b04433fecd6bd845da8816b6
ritik0201 <ritikkvs2177@gmail.com>
2026-04-02 01:13:44 +0530

    upate

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
         setIsVisible(true);
       }
 
-      setLastScrollY(currentScrollY);
+      setLastScrollY(currentScrollY); //
     };
 
     window.addEventListener("scroll", handleScroll, { passive: true });
